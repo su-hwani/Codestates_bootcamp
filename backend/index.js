341272ee265403450efef2df482e68ac7e14aff6
wjdtnghks123 <wjdtnghks123@nate.com>
2023-07-03 21:02:42 +0900

    [Feat]환경설정 세팅하기

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+// 서울시 공공데이터 포탈 인증키: 557a527753776a6439336269685878
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import nodemon from 'nodemon'
+
+const port = 8080
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config()
+
+mongoose
+  .connect(
+    process.env.URL,{}
+  )
+  .then(() => console.log('MongoDB conected'))
+  .catch((err) => {
+    console.log(err);
+  });
+  
+app.listen(port)
+console.log(`server running at http ${port}\n`)
+
